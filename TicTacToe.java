6ca6e273e61299910abfddf9588244193285ca37
abaaqielbehardien@gmail.com <abaaqielbehardien@gmail.com>
2020-09-18 17:11:34 +0200

    initial commit | tic-tac-toe | first java program | able to print layout | able to print x in selected space | first time multi-dimensional array | firs time using scanner | for loops | switch cases | if statements | functions with params and passing arguments

---
+++ TicTacToe.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class TicTacToe {
+  public static void main(String[] args) {
+
+    char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
+                          {'-', '+', '-', '+', '-'},
+                          {' ', '|', ' ', '|', ' '},
+                          {'-', '+', '-', '+', '-'},
+                          {' ', '|', ' ', '|', ' '}};
+
+      Scanner scan = new Scanner(System.in);
+      System.out.println("Enter your placement (1-9):");
+
+      int position = scan.nextInt();
+
+      placePiece(gameBoard, position, "player");
+
+      printGameBoard(gameBoard);
+  }
+
+  public static void printGameBoard(char[][] gameBoard) {
+    for(char[] row : gameBoard) {
+      for(char c : row) {
+        System.out.print(c);
+      }
+      System.out.println();
+    }
+
+  }
+
+  public static void placePiece(char[][] gameBoard, int position, String user) {
+
+    char symbol = ' ';
+
+    if (user.equals("player")) {
+      symbol = 'X';
+    } else if (user.equals("cpu")) {
+      symbol = 'O';
+    }
+
+    switch (position) {
+      case 1:
+        gameBoard[0][0] = symbol;
+        break;
+      case 2:
+        gameBoard[0][2] = symbol;
+        break;
+      case 3:
+        gameBoard[0][4] = symbol;
+        break;
+      case 4:
+        gameBoard[2][0] = symbol;
+        break;
+      case 5:
+        gameBoard[2][2] = symbol;
+        break;
+      case 6:
+        gameBoard[2][4] = symbol;
+        break;
+      case 7:
+        gameBoard[4][0] = symbol;
+        break;
+      case 8:
+        gameBoard[4][2] = symbol;
+        break;
+      case 9:
+        gameBoard[4][4] = symbol;
+        break;
+    }
+  }
+}
